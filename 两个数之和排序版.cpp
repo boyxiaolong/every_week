2b2ab1d23dcb80456662124dcfe50f544ad3f9c1
boyhailong <boyhailong@gmail.com>
2020-06-27 13:09:50 +0800

    Update 两个数之和排序版.cpp

--- 两个数之和排序版.cpp
+++ 两个数之和排序版.cpp
@@ -8,13 +8,11 @@ public:
 	int find_index(vector<int>& nums, int begin, int end, int value)
 	{
 		if (begin == end) {
-			//printf("equal %d\n", begin);
 			return begin;
 		}
 		int mid = (begin + end) / 2;
 		int mid_value = nums[mid];
 		if (mid_value == value){
-			//printf("mid %d\n", mid);
 			return mid;
 		}
 		if (mid_value < value) {
@@ -25,23 +23,41 @@ public:
 	}
 	vector<int> twoSum(vector<int>& nums, int target) {
 		vector<int> res;
+		res.resize(2, -1);
 		if (nums.empty())
 		{
 			return res;
 		}
 
-		std::sort(nums.begin(), nums.end());
-		for (int i = 0; i < nums.size(); ++i)
+		auto new_vec = nums;
+		std::sort(new_vec.begin(), new_vec.end());
+		for (int i = 0; i < new_vec.size(); ++i)
 		{
-			int cur_num = nums[i];
+			int cur_num = new_vec[i];
 			int left = target - cur_num;
 			//printf("cur i %d left %d\n", i, left);
-			int index = find_index(nums, i + 1, nums.size() - 1, left);
+			int index = find_index(new_vec, i + 1, new_vec.size() - 1, left);
 			//printf("find index %d value %d\n", index, nums[index]);
-			if (nums[index] == left)
+			if (new_vec[index] == left)
 			{
-				res.push_back(i);
-				res.push_back(index);
+				int sum = 0;
+				for (int j = 0; j < nums.size(); ++j)
+				{
+						if (nums[j] == new_vec[i] && res[0] == -1)
+						{
+							res[0] = j;
+							++sum;
+						}
+						else if (nums[j] == new_vec[index] && res[1] == -1)
+						{
+							res[1] = j;
+							++sum;
+						}
+						if (sum == 2)
+						{
+							break;
+						}
+				}
 				break;
 			}
 		}
@@ -53,8 +69,7 @@ int main()
 {
 	vector<int> nums;
 	nums.push_back(3);
-	nums.push_back(2);
-	nums.push_back(4);
+	nums.push_back(3);
 	Solution s;
 	s.twoSum(nums, 6);
 }
