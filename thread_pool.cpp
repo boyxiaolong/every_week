a32f124cad136178e214dc041fcd8f5fe9dfc5b8
zhaohailong <zhaohailong@digisky.com>
2020-10-15 15:10:32 +0800

    add

---
+++ thread_pool.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <utility>
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <atomic>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+class TaskData
+{
+public:
+	int data;
+};
+class WorkerThread
+{
+public:
+	virtual ~WorkerThread()
+	{
+		if (thd_)
+		{
+			thd_->join();
+			delete thd_;
+		}
+	}
+	void run()
+	{		
+		while (is_runing_)
+		{
+			std::queue<TaskData> temp;
+			{
+				std::unique_lock<std::mutex> guard(task_lock_);
+				if (!tasks_.empty())
+				{
+					std::swap(temp, tasks_);
+				}
+			}
+			{
+				std::unique_lock<std::mutex> guard(thread_lock_);
+				if (temp.empty() && is_runing_)
+				{
+					con_.wait(guard, [] {std::chrono::milliseconds(10); return true; });
+					continue;
+				}
+			}
+			while (!temp.empty())
+			{
+				TaskData& t = temp.front();
+				temp.pop();
+				printf("task id:%d\n", t.data);
+			}
+		}
+	}
+
+	void start()
+	{
+		thd_ = new std::thread(&WorkerThread::run, this);
+	}
+
+	bool push(TaskData& data) {
+		bool is_notify = false;
+		{
+			std::unique_lock<std::mutex> guard(task_lock_);
+			int total_size = tasks_.size();
+			if (total_size >= max_queue_size_)
+			{
+				printf("max size\n");
+				return false;
+			}
+
+			is_notify = total_size == 0;
+			tasks_.push(data);
+		}
+		if (is_notify)
+		{
+			std::unique_lock<std::mutex> guard(thread_lock_);
+			con_.notify_one();
+		}
+	}
+	void wait()
+	{
+		if (thd_)
+		{
+			thd_->join();
+		}
+	}
+private:
+	std::queue<TaskData> tasks_;
+	bool is_runing_ = true;
+	std::mutex task_lock_;
+	std::mutex thread_lock_;
+	std::condition_variable con_;
+	int max_queue_size_ = 100;
+	std::thread* thd_ = NULL;
+};
+
+bool is_running = true;
+int main()
+{
+	WorkerThread w;
+	w.start();
+	for (int i = 0; i < 999;++i)
+	{
+		TaskData t;
+		t.data = i;
+		w.push(t);
+	}
+	
+	while (is_running)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+}
