ab910235d21eff0b2c8df1fd5b06021e4ecd1b34
allenzhao <allen__zhao@hotmail.com>
2021-11-12 14:14:58 +0800

    add

--- read_xml_ip_port.py
+++ read_xml_ip_port.py
@@ -1,9 +1,12 @@
 import os
 from xml.dom.minidom import parse
 import xml.etree.ElementTree as ET
-add_index = 1
+import socket
+
+add_index = 0
 
 def cat_xml(bin_dir, res_dir):
+    local_ip = socket.gethostbyname(socket.gethostname())
     for root, dirs, files in os.walk(rootdir):
         if 'server' not in root:
             continue
@@ -12,24 +15,30 @@ def cat_xml(bin_dir, res_dir):
             if 'config.xml' not in file:
                 continue
             xml_path = os.path.join(root, file)
-            print('xml path', root, file, dirs)
+            print('xml path', root, file)
             #xmldoc = ET.ElementTree(xml_path)
             xmldoc = parse(xml_path)
             inet_c_list = xmldoc.getElementsByTagName('inet_c')
             if len(inet_c_list) > 0:
                 c_ip = inet_c_list[0].attributes['IP'].value
                 c_port = inet_c_list[0].attributes['Port'].value
-                print("cIP : ", c_ip)
-                print("cPort : ", c_port)
                 inet_c_list[0].attributes['Port'].value = str(int(c_port) + add_index)
+                inet_c_list[0].attributes['IP'].value = local_ip
 
             inet_s_list = xmldoc.getElementsByTagName('inet_s')
             if len(inet_s_list) > 0:
                 s_ip = inet_s_list[0].attributes['IP'].value
                 s_port = inet_s_list[0].attributes['Port'].value
-                print("sIP : ", s_ip)
-                print("sPort : ", s_port)
                 inet_s_list[0].attributes['Port'].value = str(int(s_port) + add_index)
+                inet_s_list[0].attributes['IP'].value = local_ip
+            
+            if 'loginserver' in root:
+                enet_list = xmldoc.getElementsByTagName('enet')
+                if len(enet_list) > 0:
+                    enet_list[0].attributes['IP'].value = local_ip
+                    print('set login eip', local_ip)
+
+
             new_str = xmldoc.toxml()
             #xmldoc.close()
             #print(new_str)
@@ -43,6 +52,7 @@ def cat_xml(bin_dir, res_dir):
             myfile.write(new_str)
             myfile.close()
 
-rootdir = r'D:\auto_explore_dir\bin\Release'
-res_dir = r'D:\auto_explore_dir\bin\Debug'
+rootdir = r'D:\test_config\server_config'
+res_dir = r'D:\test_config\server_config_1'
+
 cat_xml(rootdir, res_dir)
